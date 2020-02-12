824d5b60ddafa78bfdf6c6f09ed962d600daeb49
dmlvr <lvr@dmlvr.ru>
2020-02-12 17:21:08 +0300

    2020-02-12 правки по комментариям наставника

--- js/stat.js
+++ js/stat.js
@@ -11,8 +11,8 @@ var COLUMN_WIDTH = 40; // ширина колонки
 var TEXT_HEIGHT = 40; // высота строки текста
 var COLUMN_MAX_HEIGTH = 150; // максимальная высота колонки
 var onePtSize = 0; // размер одного пикселя в ед. измерения времени (рассчитывается ниже)
-var currentPositionColumn = CLOUD_X + DISTANCE_BETWEEN_COLUMS; // текущее положение колонки (рассчитывает ниже)
-var currentColorColumn = 'red';
+var maxTime = 0; // максимальное время прохождения игры, (рассчитывается ниже)
+var currentPositionColumn = 0; // текущее положение колонки (рассчитывается ниже)
 
 var renderCloud = function (ctx, x, y, color) { //  функция отрисовывающая облако
   ctx.fillStyle = color;
@@ -30,20 +30,28 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Список результатов:', 120, COLUMN_WIDTH + GAP + GAP);
 
   for (var y = 0; y < times.length; y++) {
-    if (onePtSize < times[y]) {
-      onePtSize = times[y];
+    if (maxTime < times[y]) {
+      maxTime = times[y];
     } //  находим самое большое время
   }
 
-  onePtSize = onePtSize / COLUMN_MAX_HEIGTH; // определяем размер одного PT исходя из размера максимального времени
+  onePtSize = maxTime / COLUMN_MAX_HEIGTH; // определяем размер одного PT исходя из размера максимального времени
 
   for (var i = 0; i < times.length; i++) {
-    ctx.fillStyle = currentColorColumn;
+    if (i === 0) {
+      currentPositionColumn = CLOUD_X + DISTANCE_BETWEEN_COLUMS;
+    }
+
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%)';
+    }
+
     ctx.fillRect(currentPositionColumn, CLOUD_HEIGHT - (TEXT_HEIGHT / 2) - times[i] / onePtSize, COLUMN_WIDTH, times[i] / onePtSize);
     ctx.fillStyle = 'black';
     ctx.fillText(names[i], currentPositionColumn, CLOUD_HEIGHT - GAP);
     ctx.fillText(Math.round(times[i]), currentPositionColumn, CLOUD_HEIGHT - TEXT_HEIGHT - times[i] / onePtSize);
     currentPositionColumn += (COLUMN_WIDTH + DISTANCE_BETWEEN_COLUMS);
-    currentColorColumn = 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%)';
   }
 };
